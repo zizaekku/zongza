ec84fed65aedb595269f182d2edd579c2677d4c7
JiwooAn <jiyouan9@gmail.com>
2022-11-01 14:55:30 +0900

    initial commit

---
+++ src/main/java/zizeaku/zongza/ZongzaApplication.java
@@ -0,0 +1,13 @@
+package zizeaku.zongza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZongzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZongzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zizeaku/zongza/ZongzaApplicationTests.java
@@ -0,0 +1,13 @@
+package zizeaku.zongza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZongzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
