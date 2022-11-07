a850efe48ad6a6742818037f2b7805716aaeeb83
JiwooAn <jiyouan9@gmail.com>
2022-11-07 15:00:02 +0900

    refact: user getter setter

--- src/main/java/zizeaku/zongza/domain/User.java
+++ src/main/java/zizeaku/zongza/domain/User.java
@@ -7,6 +7,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 @Table(name="users")
 public class User {
@@ -25,29 +30,4 @@ public class User {
     @Column(name="name")
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    
 }
