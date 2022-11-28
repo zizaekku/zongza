e232361a78925241799acb09f480773d306615a4
Jaeyeon Kim <amelia1023@naver.com>
2022-11-28 15:19:52 +0900

    css error fix & password encoding test code

--- src/test/java/zizeaku/zongza/ZongzaApplicationTests.java
+++ src/test/java/zizeaku/zongza/ZongzaApplicationTests.java
@@ -2,12 +2,16 @@ package zizeaku.zongza;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class ZongzaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("== Password Encoding ==");
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("1234"));
 	}
 
 }
